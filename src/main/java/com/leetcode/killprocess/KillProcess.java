5808b486b761f3c92f7585cd60eb8f00c392cce5
Pratik Chaudhari <chaudharipratikg@gmail.com>
2024-06-09 09:39:34 +0530

    Replace the main() method in KillProcess class with unit test(s)

--- src/main/java/com/leetcode/killprocess/KillProcess.java
+++ src/main/java/com/leetcode/killprocess/KillProcess.java
@@ -1,39 +1,15 @@
 package com.leetcode.killprocess;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.*;
 
 class KillProcess {
-
-//    public static void main(String[] args) throws IOException {
-//
-//        BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
-//
-//        System.out.print("pids: ");
-//
-//        var pids = Arrays.stream(stdin.readLine().split(",")).distinct().map(Integer::parseInt).toList();
-//
-//        System.out.print("\nppids: ");
-//
-//        var ppids = Arrays.stream(stdin.readLine().split(",")).distinct().map(Integer::parseInt).toList();
-//
-//        System.out.print("\nkill pid: ");
-//
-//        var killPid = Integer.parseInt(stdin.readLine());
-//
-//
-//        var processesToBeKilled = killProcess(pids, ppids, killPid);
-//        System.out.println(processesToBeKilled);
-//    }
 
     public List<Integer> killProcess(List<Integer> pid, List<Integer> ppid, int kill) {
 
         var parentChildProcessMapping = new HashMap<Integer, List<Integer>>();
 
-        for(int i = 0; i < ppid.size(); i++) {
-            if(!parentChildProcessMapping.containsKey(ppid.get(i))) {
+        for (int i = 0; i < ppid.size(); i++) {
+            if (!parentChildProcessMapping.containsKey(ppid.get(i))) {
                 var childPids = new ArrayList<Integer>();
                 childPids.add(pid.get(i));
                 parentChildProcessMapping.put(ppid.get(i), childPids);
@@ -48,7 +24,7 @@ class KillProcess {
 
         processesKilled.add(kill);
 
-        if(!childPidsToBeKilled.isEmpty()) {
+        if (!childPidsToBeKilled.isEmpty()) {
             processesKilled.addAll(childPidsToBeKilled);
         }
 
@@ -61,12 +37,12 @@ class KillProcess {
 
         var childProcesses = parentChildProcessMapping.get(pid);
 
-         if(childProcesses != null && !childProcesses.isEmpty()) {
-             collatedChildProcesses.addAll(childProcesses);
-             for (Integer childProcess : childProcesses) {
-                 collatedChildProcesses.addAll(getChildProcessesOf(childProcess, parentChildProcessMapping));
-             }
-         }
+        if (childProcesses != null && !childProcesses.isEmpty()) {
+            collatedChildProcesses.addAll(childProcesses);
+            for (Integer childProcess : childProcesses) {
+                collatedChildProcesses.addAll(getChildProcessesOf(childProcess, parentChildProcessMapping));
+            }
+        }
 
         return collatedChildProcesses;
     }
